7f10693e2ba92e78e6152c17188979a769d82596
Bob Whitelock <bob.whitelock1@gmail.com>
2016-08-12 22:04:05 +0100

    Colorize issue results based on language

--- main.rb
+++ main.rb
@@ -2,6 +2,7 @@
 require 'octokit'
 require 'parallel'
 require 'ruby-progressbar'
+require 'colorize'
 
 github_user = ENV['GITHUB_USER']
 github_password = ENV['GITHUB_PASSWORD']
@@ -22,7 +23,7 @@ progress_bar = ProgressBar.create(
 )
 progress_bar_mutex = Mutex.new
 
-issues_for_repos = Parallel.map(stars, in_threads: 50) do |starred_repo|
+repo_issues = Parallel.map(stars, in_threads: 50) do |starred_repo|
   issues = Octokit.list_issues(
     starred_repo.full_name,
     {state: 'open', type: 'issue', since: (DateTime.now - 7).iso8601}
@@ -31,26 +32,44 @@ issues_for_repos = Parallel.map(stars, in_threads: 50) do |starred_repo|
     !issue.pull_request && issue.comments == 0
   end
 
+  languages = Octokit.languages(starred_repo.full_name)
+  main_language = languages.first && languages.first[0] || :Text
+
   progress_bar_mutex.synchronize {
     progress_bar.increment
   }
 
-  [starred_repo.full_name, issues]
+  {
+    repo: starred_repo.full_name,
+    language: main_language,
+    issues: issues
+  }
 end
-.to_h
-.reject do |repo, issues|
-  issues.empty?
+.reject do |issue_results|
+  issue_results[:issues].empty?
 end
+
+colors = [:red, :yellow, :green, :blue, :magenta, :cyan]
+
+ordered_languages = repo_issues.
+  group_by {|result| result[:language]}.
+  sort_by {|language, results| results.length }.
+  reverse.
+  map {|grouped_language| grouped_language.first}
+
+lang_color = ordered_languages.zip(colors).to_h
 
 puts
 puts 'Found issues:'
 puts
 
-issues_for_repos.map do |repo, issues|
-  puts repo
-  puts '==============='
-  issues.map do |issue|
-    puts "#{issue.title} -- #{issue.html_url}"
+repo_issues.map do |issue_results|
+  lang = issue_results[:language]
+  color = lang_color[lang] || :default
+  puts "#{issue_results[:repo]} -- #{lang}".colorize(color)
+  puts '==============='.colorize(color)
+  issue_results[:issues].map do |issue|
+    puts "#{issue.title} -- #{issue.html_url}".colorize(color)
   end
   puts
 end
